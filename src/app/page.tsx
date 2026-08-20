cb2a170b25bdd0ca64c798a0007f89c04daf9825
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2026-08-20 08:57:16 -0300

    Remover autenticacao de edicao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,17 @@ import Header from "@/components/Header";
 import HeroSection from "@/components/sections/HeroSection";
 import GiftListSection from "@/components/sections/GiftListSection";
 import Footer from "@/components/Footer";
-import PasswordGate from "@/components/PasswordGate";
 import { cn } from "../lib/utils";
 
 export default function Home() {
   return (
-    <PasswordGate>
-      <div className={cn('flex', 'flex-col', 'min-h-[100dvh]', 'bg-background')}>
-        <Header />
-        <main className="flex-1">
-          <HeroSection />
-          <GiftListSection />
-        </main>
-        <Footer />
-      </div>
-    </PasswordGate>
+    <div className={cn('flex', 'flex-col', 'min-h-[100dvh]', 'bg-background')}>
+      <Header />
+      <main className="flex-1">
+        <HeroSection />
+        <GiftListSection />
+      </main>
+      <Footer />
+    </div>
   );
 }
